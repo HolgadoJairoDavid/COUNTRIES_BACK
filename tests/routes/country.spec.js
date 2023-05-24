18e643ae6426d32a1fb2fd2df2635e356aa75679
HolgadoJairoDavid <jairodavidholgado@gamil.com>
2023-05-24 11:12:42 -0300

    test for /countries/:id

--- tests/routes/country.spec.js
+++ tests/routes/country.spec.js
@@ -1,24 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Country, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Country, conn } = require("../../src/db.js");
 const agent = session(app);
 
-const country = {
-  name: 'Argentina',
-};
-
-describe('Country routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
-  describe('GET /countries', () => {
-    it('should get 200', () =>
-      agent.get('/countries').expect(200)
-    );
-  });
+describe("Country routes", () => {
+  it("should get Argentina by id", () =>
+    () => {agent
+      .get("/countries/ARG")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.name).to.equal("Argentine Republic");
+        expect(res.body.id).to.equal("ARG");
+        expect(res.body.image).to.equal(
+          "https://flagcdn.com/w320/ar.png"
+        );
+        expect(res.body.continent).to.equal("Americas");
+        expect(res.body.capital).to.equal("Buenos Aires");
+        expect(res.body.subregion).to.equal("South America");
+        expect(res.body.area).to.equal("2780400");
+        expect(res.body.population).to.equal(45376763);
+      })});
 });
